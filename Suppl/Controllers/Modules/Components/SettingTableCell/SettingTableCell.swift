db1190ac8871c515ec62ba1f58adcdb4706c6710
Никита Быков <nikitabykov@Nikitas-Mac.local>
2018-02-11 13:08:30 -0800

    Full proj

--- Suppl/Controllers/Modules/Components/SettingTableCell/SettingTableCell.swift
+++ Suppl/Controllers/Modules/Components/SettingTableCell/SettingTableCell.swift
@@ -1,21 +1,85 @@
 import UIKit
+import SwiftTheme
 
 class SettingTableCell: UITableViewCell {
     
     static let identifier = String(describing: TrackTableCell.self)
     
-    private var settingLabel: UILabel? = nil
-    private var settingSwitch: UISwitch? = nil
-    private var settingPicker: UIPickerView? = nil
+    private var settingLabel: UILabel = UILabel()
     
-    convenience init(picker: Bool = false) {
-        self.init()
-        settingLabel = UILabel()
-        if picker {
-            settingPicker = UIPickerView()
-            return
-        }
-        settingSwitch = UISwitch()
+    private var settingSwitch: UISwitch? = nil
+    private var settingSwitchCallback: ((_ switch:UISwitch) -> Void)? = nil
+    
+    private var settingButton: UIButton? = nil
+    private var settingButtonCallback: ((_ button:UIButton) -> Void)? = nil
+    
+    convenience init(labelText: String, buttonText:String, buttonCallback: @escaping (_ button:UIButton) -> Void) {
+        self.init(style: .default, reuseIdentifier: nil)
+        settingButtonCallback = buttonCallback
+        
+        initLabel(text: labelText)
+        
+        settingButton = UIButton(type: .system)
+        settingButton?.addTarget(self, action: #selector(settingButtonClick(_:)), for: .touchUpInside)
+        settingButton?.setTitle(buttonText, for: .normal)
+        settingButton?.theme_tintColor = "secondColor"
+        settingButton?.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(settingButton!)
+        
+        setConstraints()
     }
     
+    convenience init(labelText: String, switchValue: Bool, switchCallback: @escaping (_ switch:UISwitch) -> Void) {
+        self.init(style: .default, reuseIdentifier: nil)
+        settingSwitchCallback = switchCallback
+        
+        initLabel(text: labelText)
+        
+        settingSwitch = UISwitch()
+        settingSwitch?.addTarget(self, action: #selector(settingSwitchValueChange(_:)), for: .valueChanged)
+        settingSwitch?.setOn(switchValue, animated: false)
+        settingSwitch?.theme_onTintColor = "secondColor"
+        settingSwitch?.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(settingSwitch!)
+        
+        setConstraints()
+    }
+    
+    private func initLabel(text: String) {
+        settingLabel.text = text
+        settingLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(settingLabel)
+    }
+    
+    private func setConstraints() {
+        var constraints: [NSLayoutConstraint] = [NSLayoutConstraint]()
+        var allViews: [String:UIView] = ["label":settingLabel]
+        if let settingButton = settingButton {
+            allViews["element"] = settingButton
+        } else if let settingSwitch = settingSwitch {
+            allViews["element"] = settingSwitch
+        } else {
+            return
+        }
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label]-|", options:[], metrics: nil, views: allViews)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-[element]-|", options:[], metrics: nil, views: allViews)
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "|-[label]-[element(50)]-|", options: .alignAllCenterY, metrics: nil, views: allViews)
+        addConstraints(constraints)
+        updateConstraints()
+
+    }
+    
+    @objc private func settingButtonClick(_ sender:UIButton)
+    {
+        guard let settingButtonCallback = settingButtonCallback, let settingButton = settingButton else { return }
+        settingButtonCallback(settingButton)
+    }
+    
+    @objc private func settingSwitchValueChange(_ sender:UISwitch)
+    {
+        guard let settingSwitchCallback = settingSwitchCallback, let settingSwitch = settingSwitch else { return }
+        settingSwitchCallback(settingSwitch)
+    }
 }
+
+
--- Suppl/Controllers/Modules/Settings/Modules/SettingsGeneralViewController.swift
+++ Suppl/Controllers/Modules/Settings/Modules/SettingsGeneralViewController.swift
@@ -15,3 +15,38 @@ class SettingsGeneralViewController: UIViewController {
     }
 }
 
+extension SettingsGeneralViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        switch indexPath.row {
+        case 2:
+            cell = SettingTableCell(labelText: "Круглые миниатюры", switchValue: SettingsManager.roundIcons!) { switchElement in
+                SettingsManager.roundIcons = switchElement.isOn
+            }
+        case 1:
+            cell = SettingTableCell(labelText: "Загружать миниатюры треков", switchValue: SettingsManager.loadImages!) { switchElement in
+                SettingsManager.loadImages = switchElement.isOn
+            }
+        case 0:
+            cell = SettingTableCell(labelText: "Авто. переключение трека", switchValue: SettingsManager.autoNextTrack!) { switchElement in
+                SettingsManager.autoNextTrack = switchElement.isOn
+            }
+        case 3:
+            var themeId = SettingsManager.themesList.count <= SettingsManager.theme! ? SettingsManager.themesList.count : 1
+            cell = SettingTableCell(labelText: "Тема приложения", buttonText: SettingsManager.themesList[themeId - 1]) { button in
+                themeId = SettingsManager.themesList.count <= themeId ? 1 : themeId + 1
+                button.setTitle(SettingsManager.themesList[themeId - 1], for: .normal)
+                SettingsManager.theme = UInt(themeId)
+            }
+        default:
+            cell = UITableViewCell()
+        }
+        return cell
+    }
+    
+}
