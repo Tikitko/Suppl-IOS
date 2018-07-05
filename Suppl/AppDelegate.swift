b44f228045c9d39503fbe16d2f8bde574d50046e
Mikita Bykau <mikita_bykau@epam.com>
2018-07-05 10:54:36 +0300

    Remove old extension

--- Suppl/AppDelegate.swift
+++ Suppl/AppDelegate.swift
@@ -71,16 +71,3 @@ extension UIApplication {
         return controller
     }
 }
-
-extension UIView {
-    var parentViewController: UIViewController? {
-        var parentResponder: UIResponder? = self
-        while parentResponder != nil {
-            parentResponder = parentResponder!.next
-            if let viewController = parentResponder as? UIViewController {
-                return viewController
-            }
-        }
-        return nil
-    }
-}
