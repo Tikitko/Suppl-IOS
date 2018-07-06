9b17d67a1608e2253a1425b91a39dcdee083c65d
Mikita Bykau <mikita_bykau@epam.com>
2018-07-06 16:27:58 +0300

    Fix

--- Suppl/Model/Managers/UserDefaultsManager.swift
+++ Suppl/Model/Managers/UserDefaultsManager.swift
@@ -8,38 +8,23 @@ final class UserDefaultsManager {
     public let obj = UserDefaults()
     
     public func keyGet<T>(_ key: String) -> T? {
-        let value = obj.object(forKey: key)
-        guard let returnValue = value as? T else { return nil }
-        return returnValue
+        return obj.object(forKey: key) as? T
     }
     
     public func keySet<T>(_ key: String, value: T?) {
-        if let value = value {
-            obj.set(value, forKey: key)
-        } else {
-            obj.removeObject(forKey: key)
-        }
+        obj.set(value, forKey: key)
     }
-    
     
     private let ikey = "identifierKey"
     public var identifierKey: Int? {
-        get {
-            return keyGet(ikey)
-        }
-        set(value) {
-            keySet(ikey, value: value)
-        }
+        get { return keyGet(ikey) }
+        set(value) { keySet(ikey, value: value) }
     }
     
     private let akey = "accessKey"
     public var accessKey: Int? {
-        get {
-            return keyGet(akey)
-        }
-        set(value) {
-            keySet(akey, value: value)
-        }
+        get { return keyGet(akey) }
+        set(value) { keySet(akey, value: value) }
     }
     
 }
