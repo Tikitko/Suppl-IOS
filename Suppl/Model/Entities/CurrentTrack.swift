7aa1ac23dbcb045979d4a9c0b626c598dcf1be46
Никита Быков <bns.6587@gmail.com>
2019-05-07 13:17:50 +0300

    Fixed now playing info

--- Suppl/Model/Entities/CurrentTrack.swift
+++ Suppl/Model/Entities/CurrentTrack.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import MediaPlayer
 
 struct CurrentTrack {
     let id: String
@@ -7,4 +8,33 @@ struct CurrentTrack {
     let performer: String
     let duration: Int
     var image: UIImage?
+    
+    init(id: String, title: String, performer: String, duration: Int) {
+        self.id = id
+        self.title = title
+        self.performer = performer
+        self.duration = duration
+    }
+    
+    // MPNowPlayingInfoCenter info
+    var elapsedPlaybackTime: TimeInterval?
+    var playbackRate: Double?
+    var nowPlayingInfo: [String: Any] {
+        var nowPlayingInfo: [String: Any] = [
+            MPMediaItemPropertyTitle: title,
+            MPMediaItemPropertyArtist: performer,
+            MPMediaItemPropertyPlaybackDuration: duration
+        ]
+        if let image = image {
+            let artwork = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ in image })
+            nowPlayingInfo[MPMediaItemPropertyArtwork] = artwork
+        }
+        if let elapsedPlaybackTime = elapsedPlaybackTime {
+            nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = elapsedPlaybackTime
+        }
+        if let playbackRate = playbackRate {
+            nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = playbackRate
+        }
+        return nowPlayingInfo
+    }
 }
